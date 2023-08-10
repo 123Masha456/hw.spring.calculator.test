0069cdfd0f14f76ab2bb166125e8416422b47119
123Masha456 <xmail99@yandex.ru>
2023-08-10 08:24:38 -0700

    hw calculator

---
+++ src/main/java/pro/sky/calculator/service/CalculatorService.java
@@ -0,0 +1,6 @@
+package pro.sky.calculator.service;
+
+public interface CalculatorService {
+
+
+}
---
+++ src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -0,0 +1,10 @@
+package pro.sky.calculator.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+}
+
